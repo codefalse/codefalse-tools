cbe265b74828513bba6b96bbdada4b2a41344a3b
songxiangfeng <jakesong1018@aliyun.com>
2019-03-20 14:47:06 +0800

    init common

---
+++ codefalse-tools-common/src/main/java/com/codefalse/tools/common/StringUtils.java
@@ -0,0 +1,10 @@
+package com.codefalse.tools.common;
+
+/**
+ * <p>String类型工具类</p>
+ *
+ * @author songxiangfeng
+ * Created At 2019-03-20 14:27
+ */
+public final class StringUtils {
+}
---
+++ codefalse-tools-common/src/test/java/com/codefalse/tools/common/StringTest.java
@@ -0,0 +1,11 @@
+package com.codefalse.tools.common;
+
+/**
+ * StringUtils.java测试类
+ *
+ * @author songxiangfeng
+ * Created At 2019-03-20 14:29
+ */
+
+public class StringTest {
+}
